236d0a8cb9704446793bd34bb0e8ae69905d10be
Przemysław Białoń <przem0.b95@gmail.com>
2023-09-18 11:27:40 +0200

    moved Credentials to separate file

--- pkg/docker/config.go
+++ pkg/docker/config.go
@@ -12,20 +12,6 @@ import (
 	perrors "github.com/pkg/errors"
 )
 
-// Credentials holds the information shared between docker and the credentials store.
-type Credentials struct {
-	ServerURL string
-	Username  string
-	Secret    string
-}
-
-func (c *Credentials) AuthToken() string {
-	if c.Username != "" {
-		return c.Username + ":" + c.Secret
-	}
-	return c.Secret
-}
-
 const dockerFileFolder = ".docker"
 const AzureContainerRegistryUsername = "00000000-0000-0000-0000-000000000000"
 
