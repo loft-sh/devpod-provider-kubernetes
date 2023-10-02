a6bb675f0a1cb696a62d47a12c2f1650c045e5cb
Przemysław Białoń <przem0.b95@gmail.com>
2023-10-03 00:58:07 +0200

    implemented container status struct

---
+++ pkg/kubernetes/container_status.go
@@ -0,0 +1,43 @@
+package kubernetes
+
+import corev1 "k8s.io/api/core/v1"
+
+type ContainerStatus struct {
+	status *corev1.ContainerStatus
+}
+
+func (cs *ContainerStatus) IsReady() bool {
+	return cs.status.Ready
+}
+
+func (cs *ContainerStatus) IsWaiting() bool {
+	return cs.status.State.Waiting != nil
+}
+
+func (cs *ContainerStatus) IsTerminated() bool {
+	return cs.status.State.Terminated != nil && cs.status.State.Terminated.ExitCode != 0
+}
+
+func (cs *ContainerStatus) Succeeded() bool {
+	return cs.status.State.Terminated != nil && cs.status.State.Terminated.ExitCode == 0
+}
+
+func (cs *ContainerStatus) IsRunning() bool {
+	return cs.status.State.Running != nil
+}
+
+func (cs *ContainerStatus) IsCriticalStatus() bool {
+	return criticalStatus[cs.status.State.Waiting.Reason]
+}
+
+// criticalStatus container status
+var criticalStatus = map[string]bool{
+	"Error":                      true,
+	"Unknown":                    true,
+	"ImagePullBackOff":           true,
+	"CrashLoopBackOff":           true,
+	"RunContainerError":          true,
+	"ErrImagePull":               true,
+	"CreateContainerConfigError": true,
+	"InvalidImageName":           true,
+}
